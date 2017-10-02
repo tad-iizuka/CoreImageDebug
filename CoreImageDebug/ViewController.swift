c7f110364a115c2450997fb0ccee3fec0a1ac3f3
tad-iizuka <tad.iizuka@gmail.com>
2017-10-02 15:09:43 +0900

    Initial commit

--- CoreImageDebug/ViewController.swift
+++ CoreImageDebug/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var imageView: UIImageView!
+
+	var images: [CIImage] = []
+	let NUM = 8
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		for _ in 0..<NUM {
+			let url = URL(fileURLWithPath: Bundle.main.path(forResource: "10", ofType: "dng")!)
+			let f = CIFilter(imageURL: url, options:nil)
+			self.images.append(f!.outputImage!)
+		}
+
+		var image: CIImage!
+		let context = CIContext(options: nil)
+		let f = CIFilter(name: "CIOverlayBlendMode")
+		image = self.images[0]
+		let colorSpace = image.colorSpace
+		for i in 1..<self.images.count {
+			f?.setValue(image, forKey: kCIInputImageKey)
+			f?.setValue(self.images[i], forKey: kCIInputBackgroundImageKey)
+			image = f?.outputImage
+		}
+		let jpeg = context.jpegRepresentation(of: image!, colorSpace: colorSpace!, options: [:])
+		DispatchQueue.main.async {
+			self.imageView.image = UIImage.init(data: jpeg!)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
